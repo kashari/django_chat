204653083639287b06537f13c15a06527ed09813
Misen Kashari <misenkashari@icloud.com>
2023-01-27 14:25:00 +0100

    000006: added notifications entrypoint

--- ChatAPI/chat/models.py
+++ ChatAPI/chat/models.py
@@ -1,7 +1,7 @@
 import django
 
 django.setup()
-
+import datetime
 from django.db import models
 from users.models import User
 
@@ -19,10 +19,25 @@ class Conversation(models.Model):
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.SET_NULL,
                                null=True, related_name='message_sender')
-    text = models.CharField(max_length=200, blank=True)
+    text = models.TextField(blank=True)
     attachment = models.FileField(blank=True)
     conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE, )
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('-timestamp',)
+
+
+class Notification(models.Model):
+    to = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name='notification_to')
+    message = models.TextField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def since_when(self):
+        since_when = datetime.datetime.now() - self.timestamp
+        match since_when:
+            case since_when if since_when.seconds < 60:
+                return since_when.seconds
+            case since_when if since_when.seconds < 3600:
+                return since_when.seconds * 60
